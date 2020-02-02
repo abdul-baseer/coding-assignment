d662812eda6db8b01cb42b77bc23c58e664c5175
Abdul Baseer <muhammadabdul.baseer@careem.com>
2020-02-02 18:15:50 +0500

    3. Now how would you model a rooster?

---
+++ src/main/java/com/example/demo/Rooster.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @author baseer
+ * Date: 02/02/2020
+ */
+public class Rooster extends Chicken {
+
+    @Override
+    public String sing() {
+        return "Cock-a-doodle-doo";
+    }
+}
